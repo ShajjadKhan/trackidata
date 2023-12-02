2970ee40ee287982d4d233537ab0b145cec44968
Shajjad Khan <shajjadkhan.me@gmail.com>
2023-12-02 13:07:32 +0600

    first commit

---
+++ data.js
@@ -0,0 +1,90 @@
+const fs = require("fs");
+const path = require("path");
+
+const lib = {};
+
+lib.basedir = path.join(__dirname, "./.database/");
+// create data
+lib.create = (dir, file, data, callback) => {
+  fs.open(
+    lib.basedir + dir + "/" + file + ".json",
+    "wx",
+    (err, fileDescriptor) => {
+      if (!err && fileDescriptor) {
+        const stringdata = JSON.stringify(data);
+        fs.appendFile(fileDescriptor, stringdata, (error) => {
+          if (!error) {
+            fs.close(fileDescriptor, (err) => {
+              if (!err) {
+                callback(false);
+              } else {
+                callback("error closing the file");
+              }
+            });
+          } else {
+            callback("error ");
+          }
+        });
+      } else {
+        callback(err,"There was an Error!");
+      }
+    }
+  );
+};
+
+// read data
+lib.read = (dir, file, callback) => {
+  fs.readFile(lib.basedir + dir + "/" + file + ".json", "utf8", (err, data) => {
+    callback(err, data);
+  });
+};
+
+// update existing file
+lib.update = (dir, file, data, callback) => {
+    // file open for writing
+    fs.open(`${lib.basedir + dir}/${file}.json`, 'r+', (err, fileDescriptor) => {
+        if (!err && fileDescriptor) {
+            // convert the data to string
+            const stringData = JSON.stringify(data);
+
+            // truncate the file
+            fs.ftruncate(fileDescriptor, (err1) => {
+                if (!err1) {
+                    // write to the file and close it
+                    fs.writeFile(fileDescriptor, stringData, (err2) => {
+                        if (!err2) {
+                            // close the file
+                            fs.close(fileDescriptor, (err3) => {
+                                if (!err3) {
+                                    callback(false);
+                                } else {
+                                    callback('Error closing file!');
+                                }
+                            });
+                        } else {
+                            callback('Error writing to file!');
+                        }
+                    });
+                } else {
+                    callback('Error truncating file!');
+                }
+            });
+        } else {
+            console.log(`Error updating. File may not exist`);
+        }
+    });
+};
+
+
+// delete existing file
+lib.delete = (dir, file, callback) => {
+    // unlink file
+    fs.unlink(`${lib.basedir + dir}/${file}.json`, (err) => {
+        if (!err) {
+            callback(false);
+        } else {
+            callback(`Error deleting file`);
+        }
+    });
+};
+module.exports = lib;
